6e1395d988c7f61230c946995ae2ef69976b23a9
Anatoly Pashin <a.pashin@pik-broker.ru>
2022-01-31 16:41:52 +1000

    Используем Serializer из контейнера

---
+++ tests/Fixtures/SerializedNameQuery.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ApiClientBundle\Tests\Fixtures;
+
+use ApiClientBundle\Interfaces\ClientConfigurationInterface;
+use ApiClientBundle\Model\AbstractQuery;
+
+/**
+ * @extends AbstractQuery<SerializedNameResponse>
+ */
+class SerializedNameQuery extends AbstractQuery
+{
+    public function path(): string
+    {
+        return '/foo';
+    }
+
+    public function support(ClientConfigurationInterface $clientConfiguration): bool
+    {
+        return $clientConfiguration instanceof TestClient;
+    }
+
+    public function responseClassName(): string
+    {
+        return SerializedNameResponse::class;
+    }
+}
