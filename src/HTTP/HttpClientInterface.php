3473dd82be55ae06d0e47748f1da963b68acdd01
Kvintilyanov Aleksandr <bednyj.mops@gmail.com>
2024-01-21 04:37:53 +0300

    - add generic to QueryInterface. Now IDE(PHPStorm) know which response returned after request.

--- src/HTTP/HttpClientInterface.php
+++ src/HTTP/HttpClientInterface.php
@@ -4,8 +4,12 @@ namespace ApiClientBundle\HTTP;
 
 use ApiClientBundle\Client\QueryInterface;
 use ApiClientBundle\Client\ResponseInterface;
+use ApiClientBundle\Client\ServiceInterface;
 
 interface HttpClientInterface
 {
+    /**
+     * @param QueryInterface<ServiceInterface, ResponseInterface> $query
+     */
     public function request(QueryInterface $query): ResponseInterface;
 }
